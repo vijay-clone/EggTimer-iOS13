90a90758615acf0b82ba551c50a6e1ce543f74d8
vijay vikram singh <vijay.ectechAgmail.com>
2020-10-26 10:29:39 +0530

    completedProject

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,5 +11,41 @@ import UIKit
 class ViewController: UIViewController {
     
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    var timerDic = ["Soft" : 300, "Medium" : 420, "Hard" : 600]
+    var secondsRemaining = 0
+    var timer = Timer()
+    var seconds = 0
+    
+    
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        
+        seconds = 0
+        timer.invalidate()
+        // set title label
+        let eggTitle = sender.currentTitle!
+        titleLabel.text = eggTitle
+        
+        // set timer for the eggs
+        secondsRemaining = timerDic[eggTitle]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(action), userInfo: nil, repeats: true)
+    }
+    
+    
+    
+    @objc func action () {
+        if seconds < secondsRemaining + 1 {
+            timerProgress.progress = Float(seconds) / Float(secondsRemaining)
+            seconds += 1
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
 
+    
+    
 }
